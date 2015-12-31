f1065ccfa7bd51f43d8a16eeed9acad4069d7180
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-12-31 15:04:00 +0100

    Improve value stringification

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,22 @@
 import {title} from "change-case";
 import {is, toString} from "ramda";
 
+function stringify (thing) {
+    // ramda's toString surrounds strings with quotes, avoid that
+    return is(String, thing) ? thing : toString(thing);
+}
 function fromString (column) {
     return {
         key: column,
         formattedKey: title(column),
-        valueFormatter: toString
+        valueFormatter: stringify
     };
 }
 function fromObject (column) {
     return {
         key: column.key,
         formattedKey: column.formattedKey || title(column.key),
-        valueFormatter: column.valueFormatter || toString
+        valueFormatter: column.valueFormatter || stringify
     };
 }
 export function normalizeColumns (columns) {
